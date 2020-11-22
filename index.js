7ea68acf0d9646e25f57187e13b0be607bcbeb85
mazharultech <mazharulislam807064@gmail.com>
2020-11-22 14:16:39 +0600

    mongo database delete added

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 const  express = require('express');
-
-const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 const password = 'TbJpKG5Vd4FJt21G';
-const MongoClient = require('mongodb').MongoClient;
+
 const uri = "mongodb+srv://coding-easy:TbJpKG5Vd4FJt21G@cluster0.pi2i0.mongodb.net/eCommerceShop?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello I am working');
+    res.sendFile(__dirname + '/index.html');
 })
 
 
 client.connect(err => {
-  const collection = client.db("eCommerceShop").collection("products");
-  console.log('data received');
-  client.close();
+  const productCollection = client.db("eCommerceShop").collection("products");
+
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+  
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    productCollection.insertOne(product)
+    .then(result => {
+      console.log("product added successfully ");
+      res.send('success');
+    })
+    
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    productCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then(result => {
+      console.log(result);
+    })
+  })
+  
 });
 
 
